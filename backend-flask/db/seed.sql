62e5f7f4884aa814f51f8c77d11a153727f1f931
Marwan Radwan <marwanradwan88@gmail.com>
2023-03-18 21:21:33 +0000

    Setup postgres - Finished Live Stream week 4

---
+++ backend-flask/db/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.users (display_name, handle, cognito_user_id)
+VALUES
+  ('Marwan Khaled', 'XMARORADOTESTX' ,'MOCK'),
+  ('Andrew Bayko', 'bayko' ,'MOCK');
+
+INSERT INTO public.activities (user_uuid, message, expires_at)
+VALUES
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    'This was imported as seed data!',
+    current_timestamp + interval '10 day'
+  )
