c0910721ffa0d4d0e6dc6e0404ffc8cb389cce68
Marwan Radwan <marwanradwan88@gmail.com>
2023-03-18 09:53:50 +0000

    fixed Signout Button

--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -13,14 +13,14 @@ export default function ProfileInfo(props) {
   }
 
   const signOut = async () => {
-    const signOut = async () => {
+    
       try {
           await Auth.signOut({ global: true });
           window.location.href = "/"
       } catch (error) {
           console.log('error signing out: ', error);
       }
-    }
+    
   }
 
   const classes = () => {
