99e64b3b77213f4a00ecd43adddfbc602e47b909
Marwan Radwan <72731417+XMaroRadoX@users.noreply.github.com>
2023-03-22 17:42:44 +0000

    Testing DB again

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,12 +1,12 @@
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Marwan Khaled',"xmaroradox" ,'marwanradwan@gmail.com' ,'MOCK'),
+  ('Marwan Khaled','xmaroradox' ,'marwanradwan@gmail.com' ,'MOCK'),
   ('Andrew Bayko', 'bayko','andrewbayko@gmail.com' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'xmaroradox' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
