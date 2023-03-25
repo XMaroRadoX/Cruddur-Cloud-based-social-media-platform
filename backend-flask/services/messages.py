8caccb3d6df5c19a57770d378d56b3f1a841a60a
xmaroradox <marwanradwan88@gmail.com>
2023-03-25 00:40:29 +0000

    ✨feature: Added Messaging services

--- backend-flask/services/messages.py
+++ backend-flask/services/messages.py
@@ -2,22 +2,24 @@ from datetime import datetime, timedelta, timezone
 from lib.ddb import Ddb
 from lib.db import db
 
-
 class Messages:
-  def run(message_group_uuid, cognito_user_id):
+  def run(message_group_uuid,cognito_user_id):
     model = {
-        'errors': None,
-        'data': None
+      'errors': None,
+      'data': None
     }
 
-    sql = db.template('users', 'uuid_from_cognito_user_id')
-    my_user_uuid = db.query_value(sql, {'cognito_user_id': cognito_user_id})
-    # TODO: we're suppose to check that we have permission to access
-    # this message_group_uuid, its missing in our access pattern.
+    sql = db.template('users','uuid_from_cognito_user_id')
+    my_user_uuid = db.query_value(sql,{
+      'cognito_user_id': cognito_user_id
+    })
+
+    print(f"UUID: {my_user_uuid}")
 
     ddb = Ddb.client()
     data = Ddb.list_messages(ddb, message_group_uuid)
-    print("list_messages:", data)
+    print("list_messages")
+    print(data)
 
     model['data'] = data
     return model
