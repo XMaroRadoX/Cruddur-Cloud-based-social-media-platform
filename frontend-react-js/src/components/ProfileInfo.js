519e28fd413f82f374e89d672d8033397bbfadd7
Marwan Radwan <72731417+XMaroRadoX@users.noreply.github.com>
2023-05-03 05:41:40 +0000

    Pending changes exported from your codespace

--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -1,5 +1,6 @@
 import './ProfileInfo.css';
 import {ReactComponent as ElipsesIcon} from './svg/elipses.svg';
+import ProfileAvatar from 'components/ProfileAvatar'
 import React from "react";
 
 // [TODO] Authenication
@@ -13,15 +14,13 @@ export default function ProfileInfo(props) {
   }
 
   const signOut = async () => {
-    
-      try {
-          await Auth.signOut({ global: true });
-          window.location.href = "/"
-          localStorage.removeItem("access_token")
-      } catch (error) {
-          console.log('error signing out: ', error);
-      }
-    
+    try {
+        await Auth.signOut({ global: true });
+        window.location.href = "/"
+        localStorage.removeItem("access_token")
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
   }
 
   const classes = () => {
@@ -38,7 +37,7 @@ export default function ProfileInfo(props) {
         <button onClick={signOut}>Sign Out</button> 
       </div>
       <div className="profile-info" onClick={click_pop}>
-        <div className="profile-avatar"></div>
+        <ProfileAvatar id={props.user.cognito_user_uuid} />
         <div className="profile-desc">
           <div className="profile-display-name">{props.user.display_name || "My Name" }</div>
           <div className="profile-username">@{props.user.handle || "handle"}</div>
